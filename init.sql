5b84d8de822cb443641bb0c17e63e4a150591bcf
Özgür Önder <ozgurronderr@gmail.com>
2026-08-23 18:44:07 +0300

    PSQL'e Kurulum Bilgileri Eklendi

--- init.sql
+++ init.sql
@@ -143,3 +143,10 @@ CREATE TABLE kullanici_yetki_guncelleme_loglari (
     FOREIGN KEY (eski_rol_id) REFERENCES roller(id),
     FOREIGN KEY (yeni_rol_id) REFERENCES roller(id)
 );
+
+INSERT INTO firma (firma_kodu, firma_adi, olusturan_guncelleyen_sicil) 
+VALUES ('F001', 'Yönetim Merkezi', 'SYSTEM');
+
+-- 2. Sistem Yöneticisi Rolünün Tanımlanması (İlk kullanıcı bu rolü alacak)
+INSERT INTO roller (rol_kodu, rol_adi, olusturan_guncelleyen_sicil) 
+VALUES (1, 'Sistem Yöneticisi', 'SYSTEM');
